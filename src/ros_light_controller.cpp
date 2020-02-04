a12184502fed2ab628d7670a5a81883aee42bb7d
lucy7298@gmail.com <you@example.com>
2020-02-03 21:03:41 -0500

    ros support for two lights

--- src/ros_light_controller.cpp
+++ src/ros_light_controller.cpp
@@ -2,30 +2,31 @@
 publishes array to display on light
 **/
 #include <ros/ros.h>
-#include <std_msgs/Int8.h>
+#include <light_controller/light_command.h>
+#include <sstream>
 #include <iostream>
 #include <vector>
-#include <array> 
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "light_node_node"); 
     ros::NodeHandle n("~"); 
-    ros::Publisher pub = n.advertise<std_msgs::Int8>("light_state", 1); 
-    // 0 -> empty light 
-    // 1 -> green light 
-    // 2 -> yellow light 
-    // 3 -> red light  
+    ros::Publisher pub = n.advertise<light_controller::light_command>("light_state", 1); 
 
   while (ros::ok()){
-    int8_t message = 0;
     ros::spinOnce(); 
     // read input from cin 
     std::string inputString; 
     std::getline(std::cin, inputString);
-    message = std::stoi(inputString); 
+    std::stringstream in(inputString);
+    std::vector<int8_t> a;
+    std::string temp;
+    while(in >> temp) {
+      a.push_back(stoi(temp));
+    }
 
-    std_msgs::Int8 msg;
-    msg.data = message; 
+    light_controller::light_command msg;
+    msg.light1_status = a.at(0); 
+    msg.light2_status = a.at(1); 
     pub.publish(msg); 
 
   }
