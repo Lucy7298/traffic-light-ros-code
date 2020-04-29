3d89a9aa5f5a2c311f02ea6c56592b1fe8257777
Yunxing <yunxingl@github.mit.edu>
2020-04-28 23:47:49 -0400

    final version of code

--- src/ros_light_controller.cpp
+++ src/ros_light_controller.cpp
@@ -2,31 +2,29 @@
 publishes array to display on light
 **/
 #include <ros/ros.h>
-#include <light_controller/light_command.h>
+#include <std_msgs/UInt16.h>
 #include <sstream>
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "light_node_node"); 
     ros::NodeHandle n("~"); 
-    ros::Publisher pub = n.advertise<light_controller::light_command>("light_state", 1); 
+    ros::Publisher pub = n.advertise<std_msgs::UInt16>("light_state", 1); 
 
   while (ros::ok()){
     ros::spinOnce(); 
     // read input from cin 
     std::string inputString; 
     std::getline(std::cin, inputString);
-    std::stringstream in(inputString);
-    std::vector<int8_t> a;
-    std::string temp;
-    while(in >> temp) {
-      a.push_back(stoi(temp));
+    std_msgs::UInt16 msg;
+    int running_total = 0; 
+    for (int i = 0; i < 4; i++) {
+      int status_i = atoi(&inputString.at(i*2)); 
+      running_total += (int) status_i * pow(4, i); 
     }
-
-    light_controller::light_command msg;
-    msg.light1_status = a.at(0); 
-    msg.light2_status = a.at(1); 
+    msg.data = running_total; 
     pub.publish(msg); 
 
   }
